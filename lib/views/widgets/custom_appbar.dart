2e9d2b32fae8ddae521c1bf5f57f91222bb57957
Mina Nader <minanader648@gmail.com>
2026-03-02 00:54:13 +0200

    create appbar

---
+++ lib/views/widgets/custom_appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:my_note/views/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Text("Notes", style: TextStyle(fontSize: 30)),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
